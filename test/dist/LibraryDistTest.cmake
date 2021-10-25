b953bcf4e2b8a09114c74dcf99daf51f9fe0dc5f
Joerg Kreuzberger <j.kreuzberger@procitec.de>
2021-10-25 12:55:01 +0200

    Integrate SK Suggestions
    * Use CMAKE_POSITION_INDEPENDENT_CODE
    * doxygen Dependency configurable
    * Namespace for imported

--- test/dist/LibraryDistTest.cmake
+++ test/dist/LibraryDistTest.cmake
@@ -19,7 +19,7 @@ endif()
 foreach( buildtype "Release" "Debug")
 
   execute_process( COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/library-dist-test-build-${buildtype} )
-  execute_process( COMMAND ${CMAKE_COMMAND} -E env Qt5_DIR=${QT5_INSTALL_DIR} QLiteHtmlBrowser_DIR=${CMAKE_INSTALL_PREFIX} ${CMAKE_COMMAND} -G Ninja -DCMAKE_BUILD_TYPE=${buildtype}  ${CMAKE_INSTALL_PREFIX}/library-dist-test-src
+  execute_process( COMMAND ${CMAKE_COMMAND} -E env Qt5_DIR=${QT_INSTALL_DIR} QLiteHtmlBrowser_DIR=${CMAKE_INSTALL_PREFIX} ${CMAKE_COMMAND} -G Ninja -DCMAKE_BUILD_TYPE=${buildtype}  ${CMAKE_INSTALL_PREFIX}/library-dist-test-src
                    RESULT_VARIABLE BUILD_STATUS
                    WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/library-dist-test-build-${buildtype}
 
